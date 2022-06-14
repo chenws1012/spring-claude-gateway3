46dab28a1ab0a4a644e5d5919d18c43c7022f365
chenwenshun <chenws2014@icloud.com>
2022-06-14 16:12:12 +0800

    token check filter

---
+++ src/main/java/com/woody/gateway/filter/CheckTokenFilter.java
@@ -0,0 +1,60 @@
+package com.woody.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+/**
+ * Created by chenwenshun on 2022/6/14
+ */
+@Component
+public class CheckTokenFilter implements GlobalFilter, Ordered {
+
+    public static final String AUTHHEADER = "authorization";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest serverHttpRequest = exchange.getRequest();
+        ServerHttpResponse serverHttpResponse = exchange.getResponse();
+        String token = exchange.getRequest().getHeaders().getFirst(AUTHHEADER);
+        if(token == null){
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            String body = " {\n" +
+                    "  \"code\": 401,\n" +
+                    "  \"message\": \"Unauthorized\"\n" +
+                    "}";
+            return getVoidMono(serverHttpResponse, serverHttpRequest, body);
+        }
+        //todo token校验逻辑
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> getVoidMono(ServerHttpResponse serverHttpResponse, ServerHttpRequest httpRequest, String body) {
+        HttpHeaders headers = serverHttpResponse.getHeaders();
+        headers.add("Access-Control-Allow-Origin", Optional.ofNullable(httpRequest.getHeaders().getFirst("origin")).orElse("*") );
+        headers.add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, *");
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Content-Type", "application/json;charset=UTF-8");
+        
+        DataBuffer dataBuffer = serverHttpResponse.bufferFactory().wrap(body.getBytes());
+        return serverHttpResponse.writeWith(Flux.just(dataBuffer));
+    }
+
+    @Override
+    public int getOrder() {
+        return -100;
+    }
+}
