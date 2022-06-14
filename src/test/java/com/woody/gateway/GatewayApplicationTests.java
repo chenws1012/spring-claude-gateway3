17e67c4af33610d4d91df20c4ac88cee3486de94
chenwenshun <chenws2014@icloud.com>
2022-06-14 10:15:55 +0800

    init

---
+++ src/test/java/com/woody/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woody.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
