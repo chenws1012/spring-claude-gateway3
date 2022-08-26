70b654ae698e312d75c73f4e51947f6cdb2e2b46
chenwenshun <chenws2014@icloud.com>
2022-08-26 21:29:39 +0800

    RateLimiter

---
+++ src/main/java/com/woody/gateway/util/TokenKeyResolver.java
@@ -0,0 +1,24 @@
+package com.woody.gateway.util;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import static com.woody.gateway.filter.CheckTokenFilter.AUTHHEADER;
+
+/**
+ * Created by chenwenshun on 2022/8/26
+ */
+@Component
+public class TokenKeyResolver implements KeyResolver {
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        String token = exchange.getRequest().getHeaders().getFirst(AUTHHEADER);
+        if(StringUtils.isEmpty(token)){
+            return Mono.empty();
+        }
+        return Mono.just(token);
+    }
+}
